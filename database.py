3ed4a65c66063136911704cf76ab0135e84db795
Marco <margsjonson@gmail.com>
2024-06-28 17:52:55 -0500

    Add start file for Render deployment

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = "mysql+pymysql://root:@localhost:3306/ejemplobd2"
+URL_DATABASE = "mysql+pymysql://aaa2f3_marco:infoplus1234@mysql8010.site4now.net:3306/db_aaa2f3_marco"
 engine = create_engine(URL_DATABASE)
 SessionLocal = sessionmaker(autocommit= False , autoflush=False, bind=engine)
 Base = declarative_base()
